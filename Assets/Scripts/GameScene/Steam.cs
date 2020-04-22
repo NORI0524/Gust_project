f9bd77ffe7c1b44bd6dc67c53ac89c7c7311afc4
ikegawa <kd1229058@st.kobedenshi.ac.jp>
2020-04-22 10:46:13 +0900

    【GameScene】　湯気のスポーン処理を修正、オブヘクトを指定した秒数で透明にするテンプレクラスの作成

--- Assets/Scripts/GameScene/Steam.cs
+++ Assets/Scripts/GameScene/Steam.cs
@@ -6,25 +6,28 @@ public class Steam : BaseCompornent
 {
     private const float SteamMove = 0.005f;
 
-    float Alpha = 0.5f;
+    Transparent transparent;
+    SpawnFactory fac;
 
     // Start is called before the first frame update
     void Start()
     {
         var anim = GetComponent<Animator>();
         anim.SetBool("steamFlag", true);
+
+        transparent = GetComponent<Transparent>();
+
+        var obj = GameObject.Find("SteamManager");
+        fac = obj.GetComponent<SpawnFactory>();
     }
 
     // Update is called once per frame
     void Update()
     {
         PosY += SteamMove;
-
-        MaterialAlpha = Alpha;
-        Alpha -= 0.001f;
-
-        if(Alpha < 0.0f)
+        if (transparent.IsFinish())
         {
+            fac.Decrease();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/System/Transparent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparent : BaseCompornent
+{
+    Timer timer = null;
+    private bool isFinish;
+    private float frame;
+
+    public float InitAlpha = 1.0f;
+    public int TransparentSeconds = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frame = 0.0f;
+        isFinish = false;
+        timer = new Timer(TransparentSeconds);
+        timer.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.Update();
+        frame += 1.0f / timer.GetToInitSeconds() / 60.0f;
+        MaterialAlpha = Mathf.Lerp(InitAlpha, 0.0f, frame);
+        isFinish = MaterialAlpha <= 0.0f; 
+    }
+
+    public bool IsFinish() { return isFinish; }
+}
