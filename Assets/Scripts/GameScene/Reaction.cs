661c747d76ca2fda40762bf47da58b77ff40d502
katsubekeito <kd1233200@st.kobedenshi.ac.jp>
2020-05-07 23:22:45 +0900

    アニメーション修正 (キャラの近くに表示はまだ)

---
+++ Assets/Scripts/GameScene/Reaction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaction : MonoBehaviour
+{
+
+    void Update()
+    {
+        //GetComponentを用いてAnimatorコンポーネントを取り出す.
+        Animator animator = GetComponent<Animator>();
+
+        //あらかじめ設定していたintパラメーター「trans」の値を取り出す.
+        int start = animator.GetInteger("ReactionStart");
+
+        //上矢印キーを押した際にパラメータ「trans」の値を増加させる.
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            start++;
+        }
+
+        //下矢印キーを押した際にパラメータ「trans」の値を減少させる.
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            start--;
+        }
+
+        //intパラメーターの値を設定する.
+        animator.SetInteger("ReactionStart", start);
+    }
+}
