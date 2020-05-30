e64060dd6383c8a7ad5886ff10ff343f492396b6
katsubekeito <kd1233200@st.kobedenshi.ac.jp>
2020-05-31 05:59:53 +0900

    揚げ物アニメーション融合 湯気完成

---
+++ Assets/Scripts/TitleScene/TitleSteam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSteam : BaseCompornent
+{
+    private const float SteamMove = 0.02f;
+
+    Transparent transparent;
+    
+    void Start()
+    {
+        transparent = GetComponent<Transparent>();
+    }
+
+    void Update()
+    {
+        PosY += SteamMove;
+        if (PosY >= 3)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
