0d843d2fb9c8b395ee992d2c891a32d3d802673e
kd1278785 <kd1278785@st.kobedenshi.ac.jp>
2020-06-06 23:29:03 +0900

    rankの演出のパーティクル　ボタンにつけてるparticle flg onって関数呼んだらでてくる

---
+++ Assets/Scripts/ResultScene/Effect_test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_test : BaseCompornent
+{
+    ParticleSystem particle = null;
+    bool Flg = false;
+
+    private void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        particle.Pause();
+    }
+
+    public void Update()
+    {
+        if (Flg == true)
+        {
+            StartCoroutine("Particle");
+            Debug.Log("1");
+        }
+    }
+
+    IEnumerator Particle()
+    {
+        particle.Play();
+        Debug.Log("1");
+        yield return new WaitForSeconds(1);
+        particle.Stop();
+        Debug.Log("2");
+        Flg = false;
+        Debug.Log("3");
+        yield return null;
+    }
+
+    public void ParticleFlgOn()
+    {
+        Flg = true;
+        Debug.Log("0");
+    }
+
+}
