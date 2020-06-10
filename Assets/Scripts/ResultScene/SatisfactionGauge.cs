052fadef8d4741f42edee9587f4159ff0b46bff1
西江 <kd1268173@stkobedenshi.ac.jp>
2020-06-10 15:18:06 +0900

    リザルト満足度、スコアアニメーション

--- Assets/Scripts/ResultScene/SatisfactionGauge.cs
+++ Assets/Scripts/ResultScene/SatisfactionGauge.cs
@@ -2,24 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Scene = SceneFadeOutSteam;
 
 public class SatisfactionGauge : BaseCompornent
 {
     [SerializeField] Image obj = null;
+    [SerializeField] float GaugeSpeed = 0.1f;
+
+    private Scene scene;
+
+
 
     private const int GaugeMax = 3000;
     private const int GaugeMin = 0;
+    private bool animeFlg;
+    private int value;
 
+    public bool NextAnimeFlg { get { return animeFlg; } }
     // Start is called before the first frame update
     void Start()
     {
-     
+        GameObject SceneFadeOutSteamManager = GameObject.Find("SceneFadeOutSteamManager");
+        scene = SceneFadeOutSteamManager.GetComponent<SceneFadeOutSteam>();
+        value = Mathf.Clamp(3000, GaugeMin, GaugeMax);
+        obj.fillAmount = 0.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        int value = Mathf.Clamp(GameDirector.totalSatisfyValue, GaugeMin, GaugeMax);
-        obj.fillAmount = (float)value / GaugeMax;
+
+        if (scene.IsFinish)
+        {
+            //サウンド再生
+            obj.fillAmount = Mathf.Clamp(obj.fillAmount + GaugeSpeed, GaugeMin, (float)value / GaugeMax);
+
+            if (obj.fillAmount >= (float)value / GaugeMax)
+            {
+                animeFlg = true;
+            }
+        }
+
+
     }
 }
